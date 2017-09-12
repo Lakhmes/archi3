ed3d6f6ba99e02300040b54bac7e4d70ce4f89c1
Lakhmes <kosseila.lakhmes@gmail.com>
2017-09-12 13:22:05 +0200

    ajout classe voiture

--- archi3/src/main/java/com/huios/archi3/metier/Voiture.java
+++ archi3/src/main/java/com/huios/archi3/metier/Voiture.java
@@ -1,5 +1,23 @@
 package com.huios.archi3.metier;
 
 public class Voiture {
+private int numVoiture;
+private String model;
+public int getNumVoiture() {
+	return numVoiture;
+}
+public void setNumVoiture(int numVoiture) {
+	this.numVoiture = numVoiture;
+}
+public String getModel() {
+	return model;
+}
+public void setModel(String model) {
+	this.model = model;
+}
+@Override
+public String toString() {
+	return "Voiture [numVoiture=" + numVoiture + ", model=" + model + "]";
+}
 
 }
